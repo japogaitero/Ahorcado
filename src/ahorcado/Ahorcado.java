550a9ebabe9b9812b4433cf8ed6f0beb664e8b61
Victor <Victor@Vitorio.home>
2020-12-24 17:24:45 +0100

    SELECION DE PALABRA

--- src/ahorcado/Ahorcado.java
+++ src/ahorcado/Ahorcado.java
@@ -5,8 +5,10 @@
 */
 package ahorcado;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Random;
 
 /**
  *
@@ -16,11 +18,9 @@ public class Ahorcado {
     public static boolean otraPartida(String juego){
         Scanner teclado = new Scanner(System.in);
         boolean jugar=true;
-        boolean preguntaCorrecta=true;                
-        preguntaCorrecta=true;
-        while(preguntaCorrecta) {            
-            switch (juego)
-            {
+        boolean preguntaCorrecta=true;
+        while(preguntaCorrecta) {
+            switch (juego){
                 case "SI":
                     preguntaCorrecta=false;
                     break;
@@ -33,8 +33,60 @@ public class Ahorcado {
                     System.out.println("LIMITESE A CONTESTAR \"SI\" O \"NO\"" );
                     juego =teclado.next().toUpperCase();
             }
-        }        
+        }
         return jugar;
+    }
+    public static String selectorNivel(String nivel){
+        Scanner teclado = new Scanner(System.in);
+        boolean nivelCorrecto=true;
+        while(nivelCorrecto) {
+            switch (nivel){
+                case "FACIL":
+                    nivelCorrecto=false;
+                    break;
+                case "MEDIO":
+                    nivelCorrecto=false;
+                    break;
+                case "DIFICIL":
+                    nivelCorrecto=false;
+                    break;
+                default:
+                    System.out.println("LIMITESE A ESCRIBIR \"FACIL\" O \"MEDIO\" O \"DIFICIL\"" );
+                    nivel =teclado.next().toUpperCase();
+            }
+        }
+        return nivel;
+    }
+    public static String selectorPalabra(String nivel)throws FileNotFoundException {
+        File archivoFacil = new File ("src/ahorcado/facil.txt");
+        File archivoMedio = new File ("src/ahorcado/medio.txt");
+        File archivoDificil = new File ("src/ahorcado/dificil.txt");
+        Scanner entradaFacil = new Scanner (archivoFacil);
+        Scanner entradaMedio = new Scanner (archivoFacil);
+        Scanner entradaDificil = new Scanner (archivoFacil);
+        Random aleatorio = new Random();        
+        String palabras="";
+        String palabra;        
+        switch (nivel){
+            case "FACIL":                
+                palabras=entradaFacil.nextLine();
+                //String[] palabras= palabras.split(",");
+                break;
+            case "MEDIO":
+                palabras=entradaMedio.nextLine();                
+                break;
+            case "DIFICIL": 
+                palabras=entradaDificil.nextLine();
+                break;            
+        }
+        String[] arrayPalabras =palabras.split(",");
+        for (int i=0; i<arrayPalabras.length;i++){
+            palabra= arrayPalabras[i];
+            System.out.println(palabra);        
+        }        
+        palabra= arrayPalabras[aleatorio.nextInt(arrayPalabras.length)];
+        
+        return palabra;
     }
     
     /**
@@ -42,11 +94,28 @@ public class Ahorcado {
      */
     public static void main(String[] args)throws FileNotFoundException {
         Scanner teclado = new Scanner(System.in);
-        Scanner entrada = new Scanner ("src/ahorcado/facil.txt");
-        boolean jugar=true;
-        boolean preguntaCorrecta;
+        //Scanner entrada = new Scanner ("src/ahorcado/facil.txt");
+        String nivel;
         String juego;
+        String palabra;
+        boolean jugar=true;
         while (jugar){
+            /*-------------------------------------------------------------------
+            *    METODO PARA SELECCIONAR NIVEL
+            --------------------------------------------------------------------*/
+            System.out.println("SELECCIONE EL NIVEL QUE QUIERE JUGAR");
+            System.out.println("FACIL - - - MEDIO - - - DIFICIL");
+            nivel =teclado.next().toUpperCase();
+            nivel=selectorNivel(nivel);
+            System.out.println("VAS A JUGAR EN NIVEL "+nivel);
+            /*-------------------------------------------------------------------
+            *    METODO PARA SELECCIONAR PALABRA SEGUN EL NIVEL
+            --------------------------------------------------------------------*/            
+            palabra=selectorPalabra(nivel);            
+            System.out.println("la palabra para jugar es "+ palabra); 
+            /*-------------------------------------------------------------------
+            *    METODO PARA PREGUNTAR SI SE QUIERE JUGAR OTRA PARTIDA
+            --------------------------------------------------------------------*/
             System.out.println("¿DESEA JUGAR OTRA PARTIDA?");
             juego =teclado.next().toUpperCase();
             jugar=otraPartida(juego);//METODO PARA PREGUNTAR SI SE QUIERE JUGAR OTRA VEZ
